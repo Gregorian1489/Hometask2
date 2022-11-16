94049e1bdafc3d81005b09d9f1544430f86aff8f
Gregorian1489 <santijago@mail.ru>
2022-11-16 11:30:01 +0400

    ADD task15

---
+++ task15/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите цифру, обозначающую день недели");
+int day = Convert.ToInt32(Console.ReadLine());
+
+if ((day>0) && (day<6))
+{
+     Console.WriteLine("Это рабочий день");
+}
+else if ((day == 6) || (day == 7))
+{
+     Console.WriteLine("Это выходной");
+}
+else 
+{
+    Console.WriteLine("В неделе только семь дней");
+}
